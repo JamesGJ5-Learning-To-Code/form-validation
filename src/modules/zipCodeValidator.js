fcbd9acfb9394baf719254b17a934a145f8c30bf
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-05 21:29:55 +0000

    Ensure zip-code validation works correctly while the zip code input is focused or input event occurs
    Note: still need to add event listener for changing of country input

--- src/modules/zipCodeValidator.js
+++ src/modules/zipCodeValidator.js
@@ -1,8 +1,34 @@
 import GenericValidator from './genericValidator';
+import { countryZipCodeMap } from './countryZipCodeMap';
 
 export default class ZipCodeValidator extends GenericValidator {
   constructor(control, errorMessageSpan) {
     super(control, errorMessageSpan);
     this.messageWhenInvalid = 'A valid zip code for the selected country is required';
+    this.countryZipCodeMap = countryZipCodeMap;
+  }
+
+  doValidityChecks() {
+    const zipCode = this.control.value;
+    const country = this.getCountry();
+    this.checkPattern(country, zipCode);
+    super.doValidityChecks();
+  }
+
+  getCountry() {
+    const form = this.control.closest('form');
+    const countryControl = form.querySelector('.country');
+    return countryControl.value;
+  }
+
+  checkPattern(country, zipCode) {
+    if (country in this.countryZipCodeMap) {
+      const constraint = new RegExp(this.countryZipCodeMap[country]);
+      if (constraint.test(zipCode)) {
+        this.control.setCustomValidity('');
+      } else {
+        this.control.setCustomValidity(this.messageWhenInvalid);
+      }
+    }
   }
 }
