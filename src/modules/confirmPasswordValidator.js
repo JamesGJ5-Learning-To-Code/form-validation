8092abe2672bd0a3c11a637280b55016b0c71d0c
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-05 22:44:15 +0000

    Call super class correctly

--- src/modules/confirmPasswordValidator.js
+++ src/modules/confirmPasswordValidator.js
@@ -2,7 +2,7 @@ import GenericValidator from './genericValidator';
 
 export default class ConfirmPasswordValidator extends GenericValidator {
   constructor(confirmPasswordInput, passwordInput, errorMessageSpan) {
-    super(confirmPasswordInput, passwordInput, errorMessageSpan);
+    super(confirmPasswordInput, errorMessageSpan);
     this.messageWhenInvalid = 'The passwords do not match';
   }
 }
