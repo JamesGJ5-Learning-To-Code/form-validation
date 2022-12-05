eada4dd27d8d55a448bad14978934682d9f10e46
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-05 22:43:22 +0000

    Change error message

--- src/modules/confirmPasswordValidator.js
+++ src/modules/confirmPasswordValidator.js
@@ -1,9 +1,8 @@
-import ErrorHandler from './errorHandler';
+import GenericValidator from './genericValidator';
 
-export default class ConfirmPasswordValidator {
-  constructor(confirmPasswordInput, passwordInput, errorMessageDisplayer) {
-    this.confirmPasswordInput = confirmPasswordInput;
-    this.passwordInput = passwordInput;
-    this.errorHandler = new ErrorHandler(errorMessageDisplayer);
+export default class ConfirmPasswordValidator extends GenericValidator {
+  constructor(confirmPasswordInput, passwordInput, errorMessageSpan) {
+    super(confirmPasswordInput, passwordInput, errorMessageSpan);
+    this.messageWhenInvalid = 'The passwords do not match';
   }
 }
