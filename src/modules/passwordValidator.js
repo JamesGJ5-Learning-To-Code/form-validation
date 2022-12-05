daee82fdce1e51ad5842a27abea19b7ede07d04f
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-05 22:27:53 +0000

    Nit: reduce multi-line to single line string

--- src/modules/passwordValidator.js
+++ src/modules/passwordValidator.js
@@ -4,7 +4,6 @@ export default class PasswordValidator extends GenericValidator {
   constructor(passwordInput, errorMessageSpan) {
     super(passwordInput, errorMessageSpan);
     this.messageWhenInvalid =
-      'A password with at least one digit, at least one lowercase letter, at \
-        least one uppercase letter and a special character is required';
+      'A password with at least one digit, at least one lowercase letter, at least one uppercase letter and a special character is required';
   }
 }
