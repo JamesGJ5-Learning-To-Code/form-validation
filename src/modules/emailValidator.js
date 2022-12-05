988df02da39fbb4c9044f95822464960265e68e1
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-05 18:44:44 +0000

    Enable error displaying when input:email is focussed AND invalid

--- src/modules/emailValidator.js
+++ src/modules/emailValidator.js
@@ -3,6 +3,25 @@ import ErrorHandler from './errorHandler';
 export default class EmailValidator {
   constructor(input, errorMessageDisplayer) {
     this.input = input;
+    this.messageWhenInvalid = 'A valid email address is required';
     this.errorHandler = new ErrorHandler(errorMessageDisplayer);
+
+    this.initialiseInput();
+  }
+
+  initialiseInput() {
+    this.doValidityChecks = this.doValidityChecks.bind(this);
+    this.input.addEventListener('focus', this.doValidityChecks);
+    // this.input.addEventListener('input', this.doValidityChecks)
+  }
+
+  doValidityChecks() {
+    if (!this.input.checkValidity()) {
+      this.displayError();
+    };
+  }
+
+  displayError() {
+    this.errorHandler.errorMessage = this.messageWhenInvalid;
   }
 }
--- src/modules/errorHandler.js
+++ src/modules/errorHandler.js
@@ -2,4 +2,27 @@ export default class ErrorHandler {
   constructor(errorMessageDisplayer) {
     this.errorMessageDisplayer = errorMessageDisplayer;
   }
+
+  set errorMessage(value) {
+    this.displayErrorMessage(value);
+    if (value) {
+      this.activate();
+    } else {
+      this.deactivate();
+    }
+  }
+
+  displayErrorMessage(message) {
+    this.errorMessageDisplayer.textContent = message;
+  };
+
+  activate() {
+    // console.log(this);
+    // console.log(this.errorMessageDisplayer);
+    this.errorMessageDisplayer.classList.add('active');
+  }
+
+  deactivate() {
+    this.errorMessageDisplayer.classList.remove('active');
+  }
 }
