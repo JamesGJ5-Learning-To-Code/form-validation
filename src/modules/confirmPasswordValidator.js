27ace37c615209b2a5f79c6717918715ad8fde25
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-05 22:45:08 +0000

    Finish setting properties in constructor

--- src/modules/confirmPasswordValidator.js
+++ src/modules/confirmPasswordValidator.js
@@ -4,5 +4,6 @@ export default class ConfirmPasswordValidator extends GenericValidator {
   constructor(confirmPasswordInput, passwordInput, errorMessageSpan) {
     super(confirmPasswordInput, errorMessageSpan);
     this.messageWhenInvalid = 'The passwords do not match';
+    this.passwordInput = passwordInput;
   }
 }
