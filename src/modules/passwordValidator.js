a2d3a8c59fff9616006f0f5a4013d54335ad7f85
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-05 22:38:52 +0000

    Add TODO

--- src/modules/passwordValidator.js
+++ src/modules/passwordValidator.js
@@ -3,6 +3,7 @@ import GenericValidator from './genericValidator';
 export default class PasswordValidator extends GenericValidator {
   constructor(passwordInput, errorMessageSpan) {
     super(passwordInput, errorMessageSpan);
+    // TODO: clarify what the special characters are
     this.messageWhenInvalid =
       'An 8-16 character password is required with at least one of each of: a digit, a lowercase letter, an uppercase letter and a special character.';
   }
