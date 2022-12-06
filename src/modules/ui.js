172ae246cf50550e36fe76b89bd10f1d5751c859
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-06 01:47:17 +0000

    Perform refactor

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -9,36 +9,34 @@ export default class UI {
   static validatedElements = [];
 
   static loadValidators() {
-    const emailInput = document.getElementById('email');
-    const emailErrorMessageSpan = document.getElementById('email-error-message');
-    UI.validatedElements.push(emailInput);
-    UI.loadEmailValidator(emailInput, emailErrorMessageSpan);
+    UI.processInput('email');
+    UI.processInput('country');
+    UI.processInput('zip-code');
+    UI.processInput('password');
+    UI.processInput('confirm-password');
+  }
 
-    const countryInput = document.getElementById('country');
-    const countryErrorMessageSpan = document.getElementById('country-error-message');
-    UI.validatedElements.push(countryInput);
-    UI.loadCountryValidator(countryInput, countryErrorMessageSpan);
-
-    const zipCodeInput = document.getElementById('zip-code');
-    const zipCodeErrorMessageSpan = document.getElementById('zip-code-error-message');
-    UI.validatedElements.push(zipCodeInput);
-    UI.loadZipCodeValidator(zipCodeInput, zipCodeErrorMessageSpan);
-
-    const passwordInput = document.getElementById('password');
-    const passwordInputErrorMessageSpan = document.getElementById('password-error-message');
-    UI.validatedElements.push(passwordInput);
-    UI.loadPasswordValidator(passwordInput, passwordInputErrorMessageSpan);
-    
-    const confirmPasswordInput = document.getElementById('confirm-password');
-    const confirmPasswordErrorMessageSpan = document.getElementById(
-      'confirm-password-error-message'
-    );
-    UI.validatedElements.push(confirmPasswordInput);
-    UI.loadConfirmPasswordValidator(
-      confirmPasswordInput,
-      passwordInput,
-      confirmPasswordErrorMessageSpan
-    );
+  static processInput(inputID) {
+    const inputProcessed = document.getElementById(inputID);
+    const errorMessageSpan = document.getElementById(`${inputID}-error-message`);
+    UI.validatedElements.push(inputProcessed);
+    switch (inputID) {
+      case 'email':
+        UI.loadEmailValidator(inputProcessed, errorMessageSpan);
+        break;
+      case 'country':
+        UI.loadCountryValidator(inputProcessed, errorMessageSpan);
+        break;
+      case 'zip-code':
+        UI.loadZipCodeValidator(inputProcessed, errorMessageSpan);
+        break;
+      case 'password':
+        UI.loadPasswordValidator(inputProcessed, errorMessageSpan);
+        break;
+      case 'confirm-password':
+        const passwordInput = document.getElementById('password');
+        UI.loadConfirmPasswordValidator(inputProcessed, passwordInput, errorMessageSpan);
+    }
   }
 
   static loadEmailValidator(input, errorMessageSpan) {
